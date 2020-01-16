8264cea7e39226d4d8bcd965ebf6e9f996e4f83d
unknown <viola4lfe@gmail.com>
2020-01-16 17:22:54 -0500

    updated css

--- application-testing/src/components/Display.js
+++ application-testing/src/components/Display.js
@@ -9,20 +9,25 @@ const Display = props => {
       <section className="balls-strikes">
         <div className="topRow">
           <div className="home">
-            <h2 className="home_name">At Bat</h2>
+            <h2 className="home_name">Batter Up!</h2>
             <div className="board-container">
-            <Dashboard hitCounter={props.hitCounter} foulCounter={props.foulCounter} ballCounter={props.ballCounter} strikeCounter={props.strikeCounter} state={props.state} />
-            <div className="numbers">
-            <div className="strikes">{strike}</div>
-            <div className="balls">{ball}</div>
-            <div className="hits">{hit}</div>
-            <div className="fouls">{foul}</div>  
-            </div>
+              <Dashboard
+                hitCounter={props.hitCounter}
+                foulCounter={props.foulCounter}
+                ballCounter={props.ballCounter}
+                strikeCounter={props.strikeCounter}
+                state={props.state}
+              />
+              <div className="numbers">
+                <div className="strikes">{strike}</div>
+                <div className="balls">{ball}</div>
+                <div className="hits">{hit}</div>
+                <div className="fouls">{foul}</div>
+              </div>
             </div>
           </div>
         </div>
       </section>
-     
     </div>
   );
 };
