a45534d8e9d1cb3eec13179665981d2f9788f800
unknown <viola4lfe@gmail.com>
2020-01-16 16:12:36 -0500

    got buttons working

--- application-testing/src/App.js
+++ application-testing/src/App.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Display from "./components/Display";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      strike: 0,
+      ball: 0,
+      hit: 0,
+      foul: 0
+    };
+  }
+
+  strikeCounter = () => {
+    this.setState({
+      strike: this.state.strike + 1
+    })
+  };
+
+  ballCounter = () => {
+    this.setState({
+      ball: this.state.ball + 1
+    })
+  };
+
+  foulCounter = () => {
+    this.setState({
+      foul: this.state.foul + 1
+    })
+  };
+
+  hitCounter = () => {
+    this.setState({
+      hit: this.state.hit + 1
+    })
+  };
+
+  render() {
+    
+    return (
+      <div className="App">
+        <Display state={this.state} strikeCounter={this.strikeCounter} ballCounter={this.ballCounter} foulCounter={this.foulCounter} hitCounter={this.hitCounter} />
+      </div>
+    );
+  }
 }
 
 export default App;
